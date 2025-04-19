f20bbd818fdc77b9844d30e6c962ec1f447cf7cc
Shahzaib056 <shahzaibsaeed056@gmail.com>
2025-04-19 10:22:34 +0500

    Add files via upload

--- Source2.cpp
+++ Source2.cpp
@@ -1,17 +1,28 @@
-//#include <iostream>
-//using namespace std;
-//void print(int arr[], int a);
-//int main() {
-//    int arr[5];
-//    cout << "Enter Five integers" << endl;
-//    for (int i = 0; i < 5; i++)
-//    {
-//        cin >> arr[i];
-//    }
-//    print(arr, 5);
-//}
-//void print(int arr[], int a) {
-//    for (int i = 0; i < a; i++)
-//        cout <<"Element at"<<i+1<<"is"<< arr[i] << " "<<endl;
-//    cout << endl;
-//}
+#include <iostream>
+using namespace std;
+void vowelCount(char arr[]);
+int main() {
+	const int size = 100;
+	char input[size]{};
+	cout << "Enter a Char Array :";
+	cin.getline( input, 100);
+
+	vowelCount(input);
+
+	return 0;
+}
+void vowelCount(char arr[]) {
+	int count = 0;
+	int x = 0;
+	while (arr[x] != '\0') {
+		char ch = arr[x];
+		if (ch =='A' || ch == 'E' || ch == 'I' || ch == 'O' ||ch == 'U' ||
+			ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+		{
+			count++;
+		}
+		x++;
+	}
+	
+	cout << "Total Vowels are :" << count << endl;
+}
